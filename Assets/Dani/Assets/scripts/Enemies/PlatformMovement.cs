0868a07b70bdc0334d731337d466f0ea2e500068
Daniel Guzman <163683203+GASMOON@users.noreply.github.com>
2024-09-23 09:49:44 -0500

    si se pudo hacer la build

--- Assets/Dani/Assets/scripts/Enemies/PlatformMovement.cs
+++ Assets/Dani/Assets/scripts/Enemies/PlatformMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Net.Sockets;
-using UnityEditor.Tilemaps;
+
 using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
